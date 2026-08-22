4631504d81c83db3b07fe99254533bdb16ec3fde
Joshua Tan <joshuatanir@gmail.com>
2026-08-23 01:50:47 +0800

    feat: implement string length using pointer arithmetic

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,14 +6,11 @@
 /*   By: joshtan <joshtan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/21 12:00:00 by joshtan           #+#    #+#             */
-/*   Updated: 2026/08/21 12:00:00 by joshtan          ###   ########.fr       */
+/*   Updated: 2026/08/21 14:52:56 by joshtan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-size_t	ft_strlen(const char *s)
-{
 	// Step 1: set up a counter starting at 0.
 
 	// Step 2: walk through 's' until the terminating '\0' is reached,
@@ -21,4 +18,12 @@ size_t	ft_strlen(const char *s)
 
 	// Step 3: return the counter (return type is size_t, so it is
 	//         always >= 0 - there's no such thing as a negative length).
+size_t	ft_strlen(const char *s)
+{
+	const char	*start;
+
+	start = s;
+	while (*s)
+		++s;
+	return (s - start);
 }
