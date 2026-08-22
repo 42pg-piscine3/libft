211fe8f4ee7d4148b6a3d1f51d28e0a1964738d7
Joshua Tan <joshuatanir@gmail.com>
2026-08-23 02:49:29 +0800

    feat: implement ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,18 +6,39 @@
 /*   By: joshtan <joshtan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/21 12:00:00 by joshtan           #+#    #+#             */
-/*   Updated: 2026/08/21 12:00:00 by joshtan          ###   ########.fr       */
+/*   Updated: 2026/08/22 22:55:57 by joshtan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-void	*ft_memset(void *s, int c, size_t n)
-{
 	// Step 1: cast 's' to an (unsigned char *) so you can write it
 	//         one byte at a time regardless of the original type.
 
 	// Step 2: loop 'n' times, writing (unsigned char)c into each byte.
 
 	// Step 3: return the original pointer 's' (not the moved cursor).
+
+/**
+ * @brief Fills the first n bytes of the memory area pointed to by s 
+ * 			with the constant byte c.
+ * 
+ * @param s Pointer to the starting address of the memory block to fill
+ * @param c Character to set  
+ * @param n Number of bytes to copy
+ * @return  
+ */
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*mem;
+	int				count;
+
+	mem = (unsigned char *)s;
+	count = 0;
+	while (count < n)
+	{
+		*mem = (unsigned char )c;
+		++mem;
+		++count;
+	}
+	return (s);
 }
